b0bec5882c732a29a6512aeaafc9a1bf5376cb69
gmgn120 <gmgn120@gmail.com>
2020-12-02 04:46:34 +0000

    [Finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -13,8 +15,9 @@ class BlogsController < ApplicationController
   
   def create
     blog = Blog.new(blog_params)
+    binding.pry
     blog.save
-    redirect_to blogs_path(blog.id)
+    redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -25,6 +28,12 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
   
   private
